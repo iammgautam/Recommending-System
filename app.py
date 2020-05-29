5249d877e07ee2bf430a218f63fb2e3d90e8e3fd
Mithilesh Gautam <iammgautam@gmail.com>
2020-05-29 21:16:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,6 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!' 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
@@ -64,10 +63,9 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         if form.email.data == 'admin@admin.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
         else:
-            flash(f'Login Unsuccessful. Please check username and password', 'danger')
+            
     return render_template('login.html', title='Login', form=form)
 
 
